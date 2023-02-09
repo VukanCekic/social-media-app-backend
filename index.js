85c7b198342cc3557934cfef6f2d453add533ac5
vukan <vukanwork@gmail.com>
2023-02-09 22:46:11 +0100

    logs and build update

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("Health Check, Success");
+});
+
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.pkdbv7t.mongodb.net/social-media-app?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
@@ -28,7 +32,10 @@ try {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Server Running on Port: ${PORT}`);
+    console.log(`Mongoose connection state: ${mongoose.connection.readyState}`);
+  });
   mongoose.set("useFindAndModify", false);
 } catch (error) {
   console.log(`${error} did not connect`);
